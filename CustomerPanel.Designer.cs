53eadf0f8d665788bd0a1faa6c9e1a414661bb7e
Mohammad-Nahid-Islam-Joy <nahidislamjoy179@gmail.com>
2026-05-07 00:32:46 +0600

    Add files via upload

---
+++ CustomerPanel.Designer.cs
@@ -0,0 +1,388 @@
+﻿namespace Food_Ordering_CRUD_App
+{
+    partial class CustomerPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            pictureBox2 = new PictureBox();
+            label6 = new Label();
+            label7 = new Label();
+            pictureBox3 = new PictureBox();
+            label8 = new Label();
+            label9 = new Label();
+            pictureBox4 = new PictureBox();
+            label10 = new Label();
+            label11 = new Label();
+            pictureBox5 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            domainUpDown1 = new DomainUpDown();
+            domainUpDown2 = new DomainUpDown();
+            button4 = new Button();
+            domainUpDown3 = new DomainUpDown();
+            button5 = new Button();
+            domainUpDown4 = new DomainUpDown();
+            button6 = new Button();
+            domainUpDown5 = new DomainUpDown();
+            button7 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(64, 37);
+            label1.Name = "label1";
+            label1.Size = new Size(86, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome, User";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(64, 85);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 50);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(179, 85);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 21);
+            label2.TabIndex = 2;
+            label2.Text = "Item";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(179, 106);
+            label3.Name = "label3";
+            label3.Size = new Size(33, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Price";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(179, 174);
+            label4.Name = "label4";
+            label4.Size = new Size(33, 15);
+            label4.TabIndex = 6;
+            label4.Text = "Price";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(179, 153);
+            label5.Name = "label5";
+            label5.Size = new Size(41, 21);
+            label5.TabIndex = 5;
+            label5.Text = "Item";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(64, 153);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(100, 50);
+            pictureBox2.TabIndex = 4;
+            pictureBox2.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(179, 239);
+            label6.Name = "label6";
+            label6.Size = new Size(33, 15);
+            label6.TabIndex = 9;
+            label6.Text = "Price";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(179, 218);
+            label7.Name = "label7";
+            label7.Size = new Size(41, 21);
+            label7.TabIndex = 8;
+            label7.Text = "Item";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(64, 218);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(100, 50);
+            pictureBox3.TabIndex = 7;
+            pictureBox3.TabStop = false;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(179, 308);
+            label8.Name = "label8";
+            label8.Size = new Size(33, 15);
+            label8.TabIndex = 12;
+            label8.Text = "Price";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(179, 287);
+            label9.Name = "label9";
+            label9.Size = new Size(41, 21);
+            label9.TabIndex = 11;
+            label9.Text = "Item";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(64, 287);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(100, 50);
+            pictureBox4.TabIndex = 10;
+            pictureBox4.TabStop = false;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(179, 383);
+            label10.Name = "label10";
+            label10.Size = new Size(33, 15);
+            label10.TabIndex = 15;
+            label10.Text = "Price";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label11.Location = new Point(179, 362);
+            label11.Name = "label11";
+            label11.Size = new Size(41, 21);
+            label11.TabIndex = 14;
+            label11.Text = "Item";
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(64, 362);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(100, 50);
+            pictureBox5.TabIndex = 13;
+            pictureBox5.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(603, 26);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 31;
+            button1.Text = "Log-out";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(701, 26);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 32;
+            button2.Text = "Cart'";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(433, 86);
+            button3.Name = "button3";
+            button3.Size = new Size(102, 23);
+            button3.TabIndex = 33;
+            button3.Text = "Add";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // domainUpDown1
+            // 
+            domainUpDown1.Location = new Point(541, 86);
+            domainUpDown1.Name = "domainUpDown1";
+            domainUpDown1.Size = new Size(41, 23);
+            domainUpDown1.TabIndex = 34;
+            domainUpDown1.Text = "1";
+            // 
+            // domainUpDown2
+            // 
+            domainUpDown2.Location = new Point(541, 154);
+            domainUpDown2.Name = "domainUpDown2";
+            domainUpDown2.Size = new Size(41, 23);
+            domainUpDown2.TabIndex = 36;
+            domainUpDown2.Text = "1";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(433, 154);
+            button4.Name = "button4";
+            button4.Size = new Size(102, 23);
+            button4.TabIndex = 35;
+            button4.Text = "Add";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // domainUpDown3
+            // 
+            domainUpDown3.Location = new Point(541, 216);
+            domainUpDown3.Name = "domainUpDown3";
+            domainUpDown3.Size = new Size(41, 23);
+            domainUpDown3.TabIndex = 38;
+            domainUpDown3.Text = "1";
+            // 
+            // button5
+            // 
+            button5.Location = new Point(433, 216);
+            button5.Name = "button5";
+            button5.Size = new Size(102, 23);
+            button5.TabIndex = 37;
+            button5.Text = "Add";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // domainUpDown4
+            // 
+            domainUpDown4.Location = new Point(541, 288);
+            domainUpDown4.Name = "domainUpDown4";
+            domainUpDown4.Size = new Size(41, 23);
+            domainUpDown4.TabIndex = 40;
+            domainUpDown4.Text = "1";
+            // 
+            // button6
+            // 
+            button6.Location = new Point(433, 288);
+            button6.Name = "button6";
+            button6.Size = new Size(102, 23);
+            button6.TabIndex = 39;
+            button6.Text = "Add";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // domainUpDown5
+            // 
+            domainUpDown5.Location = new Point(541, 363);
+            domainUpDown5.Name = "domainUpDown5";
+            domainUpDown5.Size = new Size(41, 23);
+            domainUpDown5.TabIndex = 42;
+            domainUpDown5.Text = "1";
+            // 
+            // button7
+            // 
+            button7.Location = new Point(433, 363);
+            button7.Name = "button7";
+            button7.Size = new Size(102, 23);
+            button7.TabIndex = 41;
+            button7.Text = "Add";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // CustomerPanel
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(domainUpDown5);
+            Controls.Add(button7);
+            Controls.Add(domainUpDown4);
+            Controls.Add(button6);
+            Controls.Add(domainUpDown3);
+            Controls.Add(button5);
+            Controls.Add(domainUpDown2);
+            Controls.Add(button4);
+            Controls.Add(domainUpDown1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label10);
+            Controls.Add(label11);
+            Controls.Add(pictureBox5);
+            Controls.Add(label8);
+            Controls.Add(label9);
+            Controls.Add(pictureBox4);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(pictureBox3);
+            Controls.Add(label4);
+            Controls.Add(label5);
+            Controls.Add(pictureBox2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "CustomerPanel";
+            Text = "AdminPanel";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private PictureBox pictureBox2;
+        private Label label6;
+        private Label label7;
+        private PictureBox pictureBox3;
+        private Label label8;
+        private Label label9;
+        private PictureBox pictureBox4;
+        private Label label10;
+        private Label label11;
+        private PictureBox pictureBox5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private DomainUpDown domainUpDown1;
+        private DomainUpDown domainUpDown2;
+        private Button button4;
+        private DomainUpDown domainUpDown3;
+        private Button button5;
+        private DomainUpDown domainUpDown4;
+        private Button button6;
+        private DomainUpDown domainUpDown5;
+        private Button button7;
+    }
+}
---
+++ CustomerPanel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Food_Ordering_CRUD_App
+{
+    public partial class CustomerPanel : Form
+    {
+        public CustomerPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
